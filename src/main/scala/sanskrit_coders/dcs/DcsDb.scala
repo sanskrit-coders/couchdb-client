fcc50dfc7c5b2f92524be185ed4802becbab33d6
vishvAsaH <vishvas.vasuki@gmail.com>
2017-09-17 14:43:02 -0700

    Enable json output.

--- src/main/scala/sanskrit_coders/dcs/DcsDb.scala
+++ src/main/scala/sanskrit_coders/dcs/DcsDb.scala
@@ -3,6 +3,7 @@ package sanskrit_coders.dcs
 import java.io.{File, PrintWriter}
 
 import dbSchema.dcs.{DcsBook, DcsChapter, DcsSentence, DcsWord}
+import dbUtils.jsonHelper
 import org.slf4j.LoggerFactory
 import sanskrit_coders.db.CouchdbDb
 import sanskritnlp.transliteration.transliterator
@@ -13,6 +14,7 @@ class DcsBookWrapper(book: DcsBook) {
 object dcsDb {
   val booksDb = new CouchdbDb(serverLocation = "localhost:5984", dbName = "dcs_books")
   val sentencesDb = new CouchdbDb(serverLocation = "localhost:5984", dbName = "dcs_sentences")
+
   def initialize(): Unit = {
     booksDb.initialize
     sentencesDb.initialize
@@ -23,8 +25,11 @@ object dcsDb {
   }
 
   def getBook(id: String) = booksDb.getDoc[DcsBook](id = id)
+
   def getChapter(id: String) = booksDb.getDoc[DcsChapter](id = id)
+
   def getSentence(id: String) = sentencesDb.getDoc[DcsSentence](id = id)
+
   def getSentenceLink(id: String) = f"http://vedavaapi.org:5984/dcs_sentences/sentence_$id"
 }
 
@@ -32,35 +37,45 @@ object bookConverter {
   val log = LoggerFactory.getLogger(getClass.getName)
   val iastDcsCode = "iastDcs"
 
-  def dumpBook(title: String): Unit = {
-    val outfileStr = s"/home/vvasuki/couchdb-client/data/$title.tsv"
+  def dump(title: String, outputExtension: String = "tsv"): Unit = {
+    val outfileStr = s"/home/vvasuki/couchdb-client/data/$title.$outputExtension"
     val outFileObj = new File(outfileStr)
     new File(outFileObj.getParent).mkdirs
     val destination = new PrintWriter(outFileObj)
 
     val book = dcsDb.getBookByTitle(title = title).get
     val chapters = book.chapterIds.get.map(id => dcsDb.getChapter(id = s"DcsChapter_$id").get)
-    chapters.zipWithIndex.foreach({ case (chapter: DcsChapter, chapterId: Int) =>
+    val chapterSentences = chapters.zipWithIndex.map({ case (chapter: DcsChapter, chapterId: Int) =>
       val sentences = chapter.sentenceIds.get.map(id => dcsDb.getSentence(id = s"sentence_$id").get)
-      val tsvLines = sentences.zipWithIndex.map({ case (sentence: DcsSentence, sentenceNumber: Int) =>
-        val sentence_id = chapter.sentenceIds.get.apply(sentenceNumber)
-        val analysisText = sentence.dcsAnalysisDecomposition.getOrElse(Seq(Seq(DcsWord(root = "NOTHING", dcsId = 0)))).zipWithIndex.map({case (wordGroup: Seq[DcsWord], wordGroupIndex: Int) =>
-            wordGroup.zipWithIndex.map({case (word: DcsWord, intraGroupIndex: Int) =>
-              val devWord = transliterator.transliterate(in_str = word.root, destScheme = transliterator.scriptDevanAgarI, sourceScheme = iastDcsCode)
-              s"${devWord} {${wordGroupIndex+1}.${intraGroupIndex+1}}"})
-        }).flatten.mkString(";; ")
-        val devText = transliterator.transliterate(in_str = sentence.text, destScheme = transliterator.scriptDevanAgarI, sourceScheme = iastDcsCode)
-        Seq(s"${chapterId+1}-${sentenceNumber+1}", devText, analysisText, dcsDb.getSentenceLink(id = s"sentence_$sentence_id")).mkString("\t")
-      })
-      tsvLines.foreach(destination.println)
+      outputExtension match {
+        case "tsv" => {
+          val tsvLines = sentences.zipWithIndex.map({ case (sentence: DcsSentence, sentenceNumber: Int) =>
+            val sentence_id = chapter.sentenceIds.get.apply(sentenceNumber)
+            val analysisText = sentence.dcsAnalysisDecomposition.getOrElse(Seq(Seq(DcsWord(root = "NOTHING", dcsId = 0)))).zipWithIndex.map({ case (wordGroup: Seq[DcsWord], wordGroupIndex: Int) =>
+              wordGroup.zipWithIndex.map({ case (word: DcsWord, intraGroupIndex: Int) =>
+                val devWord = transliterator.transliterate(in_str = word.root, destScheme = transliterator.scriptDevanAgarI, sourceScheme = iastDcsCode)
+                s"${devWord} {${wordGroupIndex + 1}.${intraGroupIndex + 1}}"
+              })
+            }).flatten.mkString(";; ")
+            val devText = transliterator.transliterate(in_str = sentence.text, destScheme = transliterator.scriptDevanAgarI, sourceScheme = iastDcsCode)
+            Seq(s"${chapterId + 1}-${sentenceNumber + 1}", devText, analysisText, dcsDb.getSentenceLink(id = s"sentence_$sentence_id")).mkString("\t")
+          })
+          tsvLines.foreach(destination.println)
+        }
+        case _ => {}
+      }
+      sentences
     })
+    if (outputExtension == "json") {
+      destination.println(jsonHelper.asString(chapterSentences.flatten))
+    }
 
     destination.close()
   }
 
   def main(args: Array[String]): Unit = {
     dcsDb.initialize
-//    log debug transliterator.transliterate(in_str = "siddhi", destScheme = transliterator.scriptDevanAgarI, sourceScheme = iastDcsCode)
-    dumpBook(title = "Bhāvaprakāśa")
+    //    log debug transliterator.transliterate(in_str = "siddhi", destScheme = transliterator.scriptDevanAgarI, sourceScheme = iastDcsCode)
+    dump(title = "Bhāvaprakāśa", outputExtension = "json")
   }
 }
